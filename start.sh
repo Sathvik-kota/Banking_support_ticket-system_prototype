e1b757ad6aad8a4c6efbc5cb325058e05821046d
Sathvik <kotasathvik25@gmail.com>
2025-10-28 22:07:22 +0530

    testing

--- start.sh
+++ start.sh
@@ -11,11 +11,8 @@ uvicorn sync_async_routing_API:app --host 0.0.0.0 --port 8000 &
 
 echo "Starting Sync Service on port 8001..."
 # Using the filename from your log: sync_path_microservice.py
-#uvicorn sync_path_microservice:app --host 0.0.0.0 --port 8001 &
-gunicorn -k uvicorn.workers.UvicornWorker sync_path_microservice:app \
-  --workers 2 \
-  --threads 2 \
-  --bind 0.0.0.0:8001
+uvicorn sync_path_microservice:app --host 0.0.0.0 --port 8001 &
+
 
 echo "Starting Async Service on port 8002..."
 # Using the filename from your log: async_microservice.py
